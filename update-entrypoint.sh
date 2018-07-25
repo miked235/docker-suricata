cd04d2a2814d24fc7b959d1b5c48810987f8ed18
Mike <mike.deleon235@gmail.com>
2018-07-25 11:26:20 -0400

    more echos!

--- update-entrypoint.sh
+++ update-entrypoint.sh
@@ -7,9 +7,13 @@ suricata-update
 # Get list of Suricata Pods - requires RBAC
 SURICATA_PODS=`kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -l app=suricata | grep $CHART_PREFIX`
 
+echo "Found Suricata pods:"
+echo "$SURICATA_PODS"
+
 # Reload Suricata rules
 for pod in $SURICATA_PODS
 do
+  echo "Reloading rules in $pod"
   kubectl exec $pod -c suricata -- suricatasc -c reload-rules
   sleep 10 
 done
