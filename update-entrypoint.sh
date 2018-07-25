5a7683c5c48b7027f0b735d7f634bc310a4da835
Mike <mike.deleon235@gmail.com>
2018-07-25 12:14:17 -0400

    install kubectl in update script

--- update-entrypoint.sh
+++ update-entrypoint.sh
@@ -6,13 +6,18 @@ set -e
 # Update rules
 suricata-update
 
-# Get list of Suricata Pods - requires RBAC
+# Install Kubectl - REQUIRES RBAC
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+
+# Get list of Suricata Pods - REQUIRES RBAC
 SURICATA_PODS=`kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -l app=suricata | grep $CHART_PREFIX`
 
 echo "Found Suricata pods:"
 echo "$SURICATA_PODS"
 
-# Reload Suricata rules
+# Reload Suricata rules - REQUIRES RBAC
 for pod in $SURICATA_PODS
 do
   echo "Reloading rules in $pod"
