e94b0350b969ade8e828391e9260fd6c032fae30
Mike <mike.deleon235@gmail.com>
2018-07-25 15:03:20 -0400

    verbose logs

--- update-entrypoint.sh
+++ update-entrypoint.sh
@@ -11,15 +11,19 @@ sleep 10
 suricata-update
 
 # Install Kubectl - REQUIRES RBAC
+echo "Installing kubectl..."
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
 
 # Get list of Suricata Pods - REQUIRES RBAC
+echo "Getting list of Suricata Pods..."
 SURICATA_PODS=`kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -l app=suricata | grep $CHART_PREFIX`
 
+echo 
 echo "Found Suricata pods:"
 echo "$SURICATA_PODS"
+echo 
 
 # Reload Suricata rules - REQUIRES RBAC
 for pod in $SURICATA_PODS
