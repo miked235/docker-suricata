a9892d848dd1e0c319b6e44a00d7843dc9726b72
Mike <mike.deleon235@gmail.com>
2018-10-05 13:27:05 -0400

    loop to wait for suri to start up

--- update-entrypoint.sh
+++ update-entrypoint.sh
@@ -20,6 +20,21 @@ mv ./kubectl /usr/local/bin/kubectl
 echo "Getting list of Suricata Pods..."
 SURICATA_PODS=`kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -l app=suricata | grep $CHART_PREFIX`
 
+# Try 10 times before giving up 
+COUNTER=0
+until [ "SURICATA_PODS" != "" ]; do
+  if [ $COUNTER -ge 10 ]
+    echo "Too many tries, exiting... "
+    exit 1
+  fi
+  SURICATA_PODS=`kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -l app=suricata | grep $CHART_PREFIX`
+  if [ "$SURICATA_PODS" == "" ]; then
+    echo "Didn't find any Suricata pods, trying again... (x$COUNTER)"
+  fi 
+  let COUNTER++
+  sleep 5
+done
+
 echo 
 echo "Found Suricata pods:"
 echo "$SURICATA_PODS"
